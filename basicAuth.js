f5665f85d4b1615d2ee31ce5b892647e4daf8b22
Simon Rozet <simon@rozet.name>
2010-06-30 16:39:27 +0200

    smaller if forest

--- basicAuth.js
+++ basicAuth.js
@@ -11,25 +11,24 @@ module.exports = function basicAuth(callback) {
   return function(req, res, next) {
     var authorization = req.headers.authorization;
 
-    if (authorization) {
-      var parts  = authorization.split(" ");
-      var scheme = parts[0];
+    if (!authorization)
+      unauthorized(res);
 
-      if (scheme === "Basic") {
-        var credentials = base64.decode(parts[1]).split(":");
+    var parts  = authorization.split(" ");
+    var scheme = parts[0];
 
-        if (callback(credentials[0], credentials[1]) === true) {
-          req.headers["remote_user"] = credentials[0];
-          next();
-        } else {
-          unauthorized(res);
-        }
+    if (scheme === "Basic") {
+      var credentials = base64.decode(parts[1]).split(":");
+
+      if (callback(credentials[0], credentials[1]) === true) {
+        req.headers["remote_user"] = credentials[0];
+        next();
       } else {
-        res.writeHead(400);
-        res.end();
+        unauthorized(res);
       }
     } else {
-      unauthorized(res);
+      res.writeHead(400);
+      res.end();
     }
   }
 }
